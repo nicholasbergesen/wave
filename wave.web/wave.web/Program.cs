b95ef89a447cd43c44bf63de088a7b5c59845663
Nicholas Bergesen <nicholas.za@gmail.com>
2025-11-08 19:17:13 +0900

    wave.web

---
+++ wave.web/wave.web/Program.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace wave.web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.MapStaticAssets();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            // change
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
